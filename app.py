0a33d34c88e965a77beae690a629416d3ec543b9
trpnayak <60289278+trpnayak@users.noreply.github.com>
2024-05-12 21:19:47 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+#this is a sample flask application
+
+from flask import Flask
+app= Flask(__name__)
+@app.route("/")
+def hello():
+    return "<h2>CI/CD hhh125 </h2><hr/>"
+app.run(debug=True,host="192.168.43.154",port=3000) 
